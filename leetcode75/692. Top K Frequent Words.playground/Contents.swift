60d315a82f31634da3abfa2fa8001ec44775ee8d
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-10-05 17:11:49 +0900

    - Leetcode 75 <Heap> >  692. Top K Frequent Words

---
+++ leetcode75/692. Top K Frequent Words.playground/Contents.swift
@@ -0,0 +1,57 @@
+/**
+ 692. Top K Frequent Words
+ Medium
+
+ 5255
+
+ 276
+
+ Add to List
+
+ Share
+ Given an array of strings words and an integer k, return the k most frequent strings.
+
+ Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+
+  
+
+ Example 1:
+
+ Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+ Output: ["i","love"]
+ Explanation: "i" and "love" are the two most frequent words.
+ Note that "i" comes before "love" due to a lower alphabetical order.
+ Example 2:
+
+ Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
+ Output: ["the","is","sunny","day"]
+ Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
+  
+
+ Constraints:
+
+ 1 <= words.length <= 500
+ 1 <= words[i].length <= 10
+ words[i] consists of lowercase English letters.
+ k is in the range [1, The number of unique words[i]]
+  
+
+ Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
+ */
+
+class Solution {
+    func topKFrequent(_ words: [String], _ k: Int) -> [String] {
+        var frequency = Dictionary( words.map { (String($0) , 1)}, uniquingKeysWith: +).sorted {
+            if $0.value == $1.value {
+                return $0.key < $1.key
+            } else {
+                return $0.value > $1.value
+            }
+        }.map { String($0.key) }
+
+        return Array(frequency[0..<k])
+    }
+}
+
+print(Solution().topKFrequent(["aaa","aa","a"], 1))
+
