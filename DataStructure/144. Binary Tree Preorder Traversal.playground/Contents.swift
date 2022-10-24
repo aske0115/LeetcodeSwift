f32ef4caf394915d1e9f0cc3df3494ed34fcc01a
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-10-24 14:50:18 +0900

    - leetcode Datastructure <Tree>  > 144. Binary Tree Preorder Traversal

---
+++ DataStructure/144. Binary Tree Preorder Traversal.playground/Contents.swift
@@ -0,0 +1,69 @@
+/**
+ 144. Binary Tree Preorder Traversal
+ Easy
+
+ 5230
+
+ 144
+
+ Add to List
+
+ Share
+ Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+  
+
+ Example 1:
+
+
+ Input: root = [1,null,2,3]
+ Output: [1,2,3]
+ Example 2:
+
+ Input: root = []
+ Output: []
+ Example 3:
+
+ Input: root = [1]
+ Output: [1]
+  
+
+ Constraints:
+
+ The number of nodes in the tree is in the range [0, 100].
+ -100 <= Node.val <= 100
+  
+
+ Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        var result: [Int] = []
+        
+        func preorder(_ node: TreeNode?) {
+            if node != nil {
+                result.append(node!.val)
+                preorder(node?.left)
+                preorder(node?.right)
+            }
+        }
+        
+        preorder(root)
+        return  result
+    }
+}
