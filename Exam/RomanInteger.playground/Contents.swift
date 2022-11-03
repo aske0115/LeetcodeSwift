f6de01f727dbf92bcf4cbb8c72f95e5024271a52
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-11-03 17:41:09 +0900

    - leetcode Top Interview Questions  > 13. Roman to Integer better solutions

--- Exam/RomanInteger.playground/Contents.swift
+++ Exam/RomanInteger.playground/Contents.swift
@@ -56,78 +56,129 @@
 
 
 class Solution {
-
     func romanToInt(_ s: String) -> Int {
-        //문자열을 앞에서 쪼개본다
-        //문자열이 i,x,c일경우 다음의 문자가 (v,x)(l,c)(d,m)인지 확인한다.
-        //문자열이 i,x,c가 아닐 경우 각각 치환되어질 숫자를 합한다.
-        //다 합친 결과값을 리턴해준다.
-        //1, 문자열 쪼개기
+        let dictionary:[String: String] = ["I" : "1",
+                                           "IV" : "4",
+                                           "V" : "5",
+                                           "IX" : "9",
+                                           "X" : "10",
+                                           "XL" : "40",
+                                           "L" : "50",
+                                           "XC" : "90",
+                                           "C" : "100",
+                                           "CD" : "400",
+                                           "D" : "500",
+                                           "CM" : "900",
+                                           "M" : "1000"]
         
-        var result: Int = 0
         
-        var startIndex = 0
-        while startIndex < s.count {
-            let res = checkRoman(s, startIndex: startIndex)
-            startIndex = res.lastOffeset
-            result += res.result
+        func isRoman(_ char:String) -> Bool {
+            char == "I" || char == "X" || char == "C"
+        }
+
+        var result = 0
+        var pass = false
+        var strArray = s.map { String($0) }
+        for i in 0..<strArray.count {
+            if pass {
+                pass = false
+                continue
+            }
+            print("for start \(strArray[i])")
+            if isRoman(strArray[i]) {
+                if i < strArray.count - 1, let value = dictionary[strArray[i...i+1].joined(separator:"")] {
+                    print("if for \(value)")
+                    result += Int(value)!
+                    pass = true
+                } else {
+                    print("else for \(dictionary[strArray[i]])")
+                    result += Int(dictionary[strArray[i]]!)!
+                }
+            } else {
+                print("else  \(dictionary[strArray[i]])")
+                result += Int(dictionary[strArray[i]]!)!
+            }
+            print("result = \(result)")
         }
         
         return result
     }
-    
-    func checkRoman(_ s: String , startIndex: Int) -> (result: Int, lastOffeset: Int) {
-        
-        let mapStr = s.map {
-            String($0)
-        }
-        var checkedIndex =  min(s.count , startIndex + 1)
-
-        let first = mapStr[startIndex]
-        let offset = checkedIndex == s.count ? nil :  mapStr[checkedIndex]
-    
-        if first != offset, offset != nil, checkTwoRoman(first+offset!) {
-            checkedIndex += 1
-        }
-        
-        let checkStr = mapStr[startIndex..<min(s.count,checkedIndex)].joined(separator: "")
-      
-        switch checkStr {
-        case "IV":
-            return (4, checkedIndex)
-        case "IX":
-            return (9, checkedIndex)
-        case "XL":
-            return (40, checkedIndex)
-        case "XC":
-            return (90, checkedIndex)
-        case "CD":
-            return (400, checkedIndex)
-        case "CM":
-            return (900, checkedIndex)
-        case "I":
-            return (1, checkedIndex)
-        case "V":
-            return (5, checkedIndex)
-        case "X":
-            return (10, checkedIndex)
-        case "L":
-            return (50, checkedIndex)
-        case "C":
-            return (100, checkedIndex)
-        case "D":
-            return (500, checkedIndex)
-        case "M":
-            return (1000, checkedIndex)
-        default:
-            return (0,s.count)
-        }
-    }
-    
-    func checkTwoRoman(_ s: String) -> Bool {
-        return s == "IV" || s == "IX" || s == "XL" || s == "XC" || s == "CD" || s == "CM"
-    }
 }
 
+
+//class Solution {
+//
+//    func romanToInt(_ s: String) -> Int {
+//        //문자열을 앞에서 쪼개본다
+//        //문자열이 i,x,c일경우 다음의 문자가 (v,x)(l,c)(d,m)인지 확인한다.
+//        //문자열이 i,x,c가 아닐 경우 각각 치환되어질 숫자를 합한다.
+//        //다 합친 결과값을 리턴해준다.
+//        //1, 문자열 쪼개기
+//
+//        var result: Int = 0
+//
+//        var startIndex = 0
+//        while startIndex < s.count {
+//            let res = checkRoman(s, startIndex: startIndex)
+//            startIndex = res.lastOffeset
+//            result += res.result
+//        }
+//
+//        return result
+//    }
+//
+//    func checkRoman(_ s: String , startIndex: Int) -> (result: Int, lastOffeset: Int) {
+//
+//        let mapStr = s.map {
+//            String($0)
+//        }
+//        var checkedIndex =  min(s.count , startIndex + 1)
+//
+//        let first = mapStr[startIndex]
+//        let offset = checkedIndex == s.count ? nil :  mapStr[checkedIndex]
+//
+//        if first != offset, offset != nil, checkTwoRoman(first+offset!) {
+//            checkedIndex += 1
+//        }
+//
+//        let checkStr = mapStr[startIndex..<min(s.count,checkedIndex)].joined(separator: "")
+//
+//        switch checkStr {
+//        case "IV":
+//            return (4, checkedIndex)
+//        case "IX":
+//            return (9, checkedIndex)
+//        case "XL":
+//            return (40, checkedIndex)
+//        case "XC":
+//            return (90, checkedIndex)
+//        case "CD":
+//            return (400, checkedIndex)
+//        case "CM":
+//            return (900, checkedIndex)
+//        case "I":
+//            return (1, checkedIndex)
+//        case "V":
+//            return (5, checkedIndex)
+//        case "X":
+//            return (10, checkedIndex)
+//        case "L":
+//            return (50, checkedIndex)
+//        case "C":
+//            return (100, checkedIndex)
+//        case "D":
+//            return (500, checkedIndex)
+//        case "M":
+//            return (1000, checkedIndex)
+//        default:
+//            return (0,s.count)
+//        }
+//    }
+//
+//    func checkTwoRoman(_ s: String) -> Bool {
+//        return s == "IV" || s == "IX" || s == "XL" || s == "XC" || s == "CD" || s == "CM"
+//    }
+//}
+
 var sol = Solution()
-sol.romanToInt("III")
+sol.romanToInt("MCMXCIV")
